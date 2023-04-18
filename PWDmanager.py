c6a23c51a68a9d8b758b0a5fd2b634dfbc0fa5f7
Mohammad Yosef <93715112+J0kErF@users.noreply.github.com>
2023-04-19 01:57:02 +0300

    Update PWDmanager.py

--- PWDmanager.py
+++ PWDmanager.py
@@ -6,85 +6,90 @@ from cryptography.hazmat.backends import default_backend
 backend = default_backend()
 
 
-def encrypt(message: bytes, public_key_file: str) -> bytes:
-    # Load the public key from file
-    with open(public_key_file, "rb") as key_file:
-        public_key = serialization.load_pem_public_key(
-            key_file.read(),
-            backend=default_backend()
-        )
-
-    # Encrypt the message using RSA encryption with OAEP padding
-    encrypted = public_key.encrypt(
-        message,
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
-    )
-
-    return encrypted
+def encrypt(data, shift):
+    result = ""
+    for char in data:
+        if char.isupper():
+            result += chr((ord(char) + shift - 65) % 26 + 65)
+        elif char.islower():
+            result += chr((ord(char) + shift - 97) % 26 + 97)
+        else:
+            result += char
+    return result
 
 
-def decrypt(websitename,username):
-    exit()
+def decrypt(websitename, username):
+    count = 1
+    num = 1
+    with open("data.txt", "r") as f:
+        for line in f:
+            if encrypt(line, -1*count).split(',')[0] == websitename:
+                if username == "*" or username == encrypt(line, -1*count).split(',')[1]:
+                    print(str(num)+": ")
+                    print("[*] username: "+encrypt(line, -1*count).split(',')[1])
+                    print("[*] password: "+encrypt(line, -1*count).split(',')[2])
+                    num += 1
+            count += 1
+
+
 def addPWD():
-    websitename=input("[*] enter website/app name: ")
+    websitename = input("[*] enter website/app name: ")
     print("[*] website/app name: "+websitename+"\n")
-    done=0
+    done = 0
     while done != 1:
-        username=input("[*] enter username: ")
+        username = input("[*] enter username: ")
         print("[*] username: "+username+"\n")
-        done=int(input("[*] enter 1 to save | 0 to RE-enter username "))
-    done=0
+        done = int(input("[*] enter 1 to save | 0 to RE-enter username "))
+    done = 0
     while done != 1:
-        PWD=input("[*] enter password: ")
+        PWD = input("[*] enter password: ")
         print("[*] password: "+PWD+"\n")
-        done=int(input("[*] enter 1 to save | 0 to RE-enter password "))
-    done=int(input("[*] enter 1 to save | anything to CANCEL and EXIT "))
-    if done==1:
-        line="("+websitename+","+username+","+PWD+")"
-        encrypt(line,"public_key.pem")
+        done = int(input("[*] enter 1 to save | 0 to RE-enter password "))
+    if done == 1:
+        data = websitename+","+username+","+PWD
+        count = 1
         
+        with open("data.txt", "r") as f:
+            for line in f:
+                count += 1
+        data = encrypt(line, count)
+        with open("data.txt", "a") as f:
+            f.write(data+"\n")
+
     exit()
+
+
 def revealPWD():
-    websitename=input("[*] enter website/app name: ")
+    websitename = input("[*] enter website/app name: ")
     print("[*] website/app name: "+websitename+"\n")
-    done=0
+    done = 0
     while done != 1:
-        username=input("[*] enter username (or '*' to show all users): ")
+        username = input("[*] enter username (or '*' to show all users): ")
         print("[*] username: "+username+"\n")
-        done=int(input("[*] enter 1 to proceed | 0 to RE-enter username "))
-    done=int(input("[*] enter 1 to proceed | anything to CANCEL and EXIT "))
-    if done==1:
-        decrypt(websitename,username)
+        done = int(input("[*] enter 1 to proceed | 0 to RE-enter username "))
+    if done == 1:
+        decrypt(websitename, username)
     exit()
+
 
 def boot():
     os.system('cls' if os.name == 'nt' else 'clear')
-    header="\n ______          _      _    _  _  _  _____  ______  _____      ______         ______          ______ _______ ______  \n(_____ \ /\     | |    | |  | || || |/ ___ \(_____ \(____ \    |  ___ \   /\  |  ___ \   /\   / _____|_______|_____ \ \n _____) )  \     \ \    \ \ | || || | |   | |_____) )_   \ \   | | _ | | /  \ | |   | | /  \ | /  ___ _____   _____) )\n|  ____/ /\ \     \ \    \ \| ||_|| | |   | (_____ (| |   | |  | || || |/ /\ \| |   | |/ /\ \| | (___)  ___) (_____ ( \n| |   | |__| |_____) )____) ) |___| | |___| |     | | |__/ /   | || || | |__| | |   | | |__| | \____/| |_____      | |\n|_|   |______(______(______/ \______|\_____/      |_|_____/    |_||_||_|______|_|   |_|______|\_____/|_______)     |_|\n\n\n"
-    name="BY\n╔╦╗┌─┐┬ ┬┌─┐┌┬┐┌┬┐┌─┐┌┬┐  ╦ ╦┌─┐┌─┐┌─┐┌─┐\n║║║│ │├─┤├─┤││││││├─┤ ││  ╚╦╝│ │└─┐├┤ ├┤ \n╩ ╩└─┘┴ ┴┴ ┴┴ ┴┴ ┴┴ ┴─┴┘   ╩ └─┘└─┘└─┘└  \n"
+    header = "\n ______          _      _    _  _  _  _____  ______  _____      ______         ______          ______ _______ ______  \n(_____ \ /\     | |    | |  | || || |/ ___ \(_____ \(____ \    |  ___ \   /\  |  ___ \   /\   / _____|_______|_____ \ \n _____) )  \     \ \    \ \ | || || | |   | |_____) )_   \ \   | | _ | | /  \ | |   | | /  \ | /  ___ _____   _____) )\n|  ____/ /\ \     \ \    \ \| ||_|| | |   | (_____ (| |   | |  | || || |/ /\ \| |   | |/ /\ \| | (___)  ___) (_____ ( \n| |   | |__| |_____) )____) ) |___| | |___| |     | | |__/ /   | || || | |__| | |   | | |__| | \____/| |_____      | |\n|_|   |______(______(______/ \______|\_____/      |_|_____/    |_||_||_|______|_|   |_|______|\_____/|_______)     |_|\n\n\n"
+    name = "BY\n╔╦╗┌─┐┬ ┬┌─┐┌┬┐┌┬┐┌─┐┌┬┐  ╦ ╦┌─┐┌─┐┌─┐┌─┐\n║║║│ │├─┤├─┤││││││├─┤ ││  ╚╦╝│ │└─┐├┤ ├┤ \n╩ ╩└─┘┴ ┴┴ ┴┴ ┴┴ ┴┴ ┴─┴┘   ╩ └─┘└─┘└─┘└  \n"
     print(header)
     print(name)
-    mainlist="[*] Add password\n[*] Reveal password\n[*] Delete password\n[*] EXIT\n"
+    mainlist = "[*] Add password\n[*] Reveal password\n[*] Delete password\n[*] EXIT\n"
     print(mainlist)
-    x=int(input("choose your option: "))
-    if x==1 :
+    x = int(input("choose your option: "))
+    if x == 1:
         addPWD()
-    elif x==2 :
+    elif x == 2:
         revealPWD()
-    elif x==3 :
+    elif x == 3:
         print("[*] Delete password (in MAINTAINCE)")
-    elif x==4 :
+    elif x == 4:
         exit()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     boot()
-
-
-
-
-
-
-
